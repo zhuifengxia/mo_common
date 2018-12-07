565846cef09e15d6fc56aeb2e3c8dd0263e70312
Your Name <xirizhifeng@163.com>
2018-12-07 10:08:13 +0800

    openssl

--- src/Support/EncryptTool.php
+++ src/Support/EncryptTool.php
@@ -57,4 +57,79 @@ class EncryptTool
     }
 
     /****************DES加密END*********************/
+
+
+    /****************OPENSSL加密START*********************/
+
+    /**
+     * 私钥加密内容
+     * @param $rootpath 证书所在根目录
+     * @param $data 待加密字符串
+     * @return string 加密之后字符串
+     */
+    public static function data_openssl_private_encode($rootpath,$data)
+    {
+        $private_key = file_get_contents(self::get_cart_path($rootpath, 0));
+        $pi_key = openssl_pkey_get_private($private_key);//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
+        openssl_private_encrypt($data, $encrypted, $pi_key);//私钥加密
+        $encrypted = urlencode(base64_encode($encrypted));
+        return $encrypted;
+    }
+
+    /**
+     * 公钥解密
+     * @param $rootpath 证书所在根目录
+     * @param $encrypted 待解密字符串
+     * @return mixed 解密之后的字符串
+     */
+    public static function data_openssl_public_decode($rootpath,$encrypted)
+    {
+        $public_key = file_get_contents(self::get_cart_path($rootpath, 1));
+        $pu_key = openssl_pkey_get_public($public_key);//这个函数可用来判断公钥是否是可用的
+        openssl_public_decrypt(base64_decode(urldecode($encrypted)), $decrypted, $pu_key);//公钥解密
+        return $decrypted;
+    }
+
+    /**
+     * 公钥加密
+     * @param $rootpath 证书所在根目录
+     * @param $data 待加密字符串
+     * @return string 加密之后的字符串
+     */
+    public static function data_openssl_public_encode($rootpath,$data)
+    {
+        $public_key = file_get_contents(self::get_cart_path($rootpath, 1));
+        $pu_key = openssl_pkey_get_public($public_key);//这个函数可用来判断公钥是否是可用的
+        openssl_public_encrypt($data, $encrypted, $pu_key);//公钥加密
+        $encrypted = urlencode(base64_encode($encrypted));
+        return $encrypted;
+    }
+
+    /**
+     * 私钥解密
+     * @param $rootpath 证书所在根目录
+     * @param $encrypted 待解密字符串
+     * @return mixed 解密之后的字符串
+     */
+    public static function data_openssl_private_decode($rootpath,$encrypted)
+    {
+        $private_key = file_get_contents(self::get_cart_path($rootpath, 0));
+        $pi_key = openssl_pkey_get_private($private_key);//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id
+        openssl_private_decrypt(base64_decode(urldecode($encrypted)), $decrypted, $pi_key);//私钥解密
+        return $decrypted;
+    }
+
+    /**
+     * 证书路径
+     * @param $rootpath 根目录
+     * @param int $datatpe 0私钥;1公钥
+     * @return string
+     */
+    function get_cart_path($rootpath,$datatpe=0)
+    {
+        $filepath = $rootpath . "mo/mo-common/src/Support/cart/app_" . ($datatpe ? "public" : "private") . "_key.pem";
+        return $filepath;
+    }
+    /****************OPENSSL加密END*********************/
+
 }
