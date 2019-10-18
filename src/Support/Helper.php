89c295e1ee5f9a1fecf2dfd3b7821ec3fe6ec6a8
momo <xirizhifeng@163.com>
2019-10-18 14:57:59 +0800

    date method

--- src/Support/Helper.php
+++ src/Support/Helper.php
@@ -536,4 +536,43 @@ class Helper
            }
        }
    }
+
+    /**
+     * 将指定时间戳转换为个性化的文字提示
+     * @param null $time 时间戳
+     * @return string 自定义时间提示
+     */
+    public static function mdate($time = NULL)
+    {
+        $text = '';
+        $t = time() - $time; //时间差 （秒）
+        $y = date('Y', $time) - date('Y', time());//是否跨年
+        switch ($t) {
+            case $t == 0:
+                $text = '刚刚';
+                break;
+            case $t < 60:
+                $text = $t . '秒前'; // 一分钟内
+                break;
+            case $t < 60 * 60:
+                $text = floor($t / 60) . '分钟前'; //一小时内
+                break;
+            case $t < 60 * 60 * 24:
+                $text = floor($t / (60 * 60)) . '小时前'; // 一天内
+                break;
+            case $t < 60 * 60 * 24 * 3:
+                $text = floor($time / (60 * 60 * 24)) == 1 ? '昨天' . date('H:i', $time) : '前天 ' . date('H:i', $time); //昨天和前天
+                break;
+            case $t < 60 * 60 * 24 * 30:
+                $text = floor($t / (60 * 60 * 24)) . '天前';
+                break;
+            case $t < 60 * 60 * 24 * 365 && $y == 0:
+                $text = floor($t / (60 * 60 * 24 * 30)) . '个月前';
+                break;
+            default:
+                $text = date('Y-m-d', $time); //两年以前
+                break;
+        }
+        return $text;
+    }
 }
