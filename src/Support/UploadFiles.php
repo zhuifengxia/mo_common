c9f41b9b4811f741f523062b4a89b5966a262171
momo <xirizhifeng@163.com>
2020-05-09 15:25:56 +0800

    bug fixed

--- src/Support/UploadFiles.php
+++ src/Support/UploadFiles.php
@@ -13,13 +13,14 @@ class UploadFiles
 
     /**
      * 单个文件上传
-     * @param $typelist 可以上传的文件类型
+     * @param $typelist 可以上传的文件类型;-1所有类型
      * @param $file 文件内容
      * @param $filepath 文件上传路径
+     * @param $imgfile 1图片文件；0其他文件
      * @param $viewpath 外部访问地址
      * @param $filesize 文件大小限制数字M为单位
      */
-    public static function single_file_upload($typelist,$file,$filepath,$viewpath,$filesize=0)
+    public static function single_file_upload($typelist,$file,$filepath,$isimgfile=1,$viewpath="",$filesize=0)
     {
         $result_data = [
             'status' => -1,
@@ -41,35 +42,41 @@ class UploadFiles
             }
         }
 
-
-        if (is_uploaded_file($file['tmp_name']) && in_array($file['type'], $typelist)){
-            //读取图片内容
-            $txt  = fopen($file['tmp_name'], "rb");
-            $bin  = fread($txt,$file['size']);
-            fclose($txt);
-            $pos = strpos($bin, '<?php');
-            if (!$pos){
-                //获取文件扩展名
-                $exten_name = pathinfo($file['name'], PATHINFO_EXTENSION);
-                //重新命名图片名称
-                $picname = Helper::custom_mt_uniqid() . "." . $exten_name;//重新命名文件名
-                $fpath = $filepath;
-                //路径是否存在，不存在则创建
-                if (!is_dir($fpath)) mkdir($fpath, 0777);
-                //调用move_uploaded_file（）函数，进行文件转移
-                $path = $fpath . $picname;
-                if (move_uploaded_file($file['tmp_name'], $path)) {
-                    $resultpath = $viewpath . $picname;
-                    $result_data['status']=0;
-                    $result_data['msg']='成功';
-                    $result_data['data'] = $resultpath;
+        if (is_uploaded_file($file['tmp_name'])) {
+            if ($typelist != -1 && !in_array($file['type'], $typelist)) {
+                $result_data['msg'] = "文件格式错误";
+            } else {
+                if ($isimgfile) {
+                    //读取图片内容
+                    $txt = fopen($file['tmp_name'], "rb");
+                    $bin = fread($txt, $file['size']);
+                    fclose($txt);
+                    $pos = strpos($bin, '<?php');
+                } else {
+                    $pos = false;
                 }
-            }else{
-                $result_data['msg'] = "该文件不是图片文件";
+                if (!$pos) {
+                    //获取文件扩展名
+                    $exten_name = pathinfo($file['name'], PATHINFO_EXTENSION);
+                    //重新命名图片名称
+                    $picname = Helper::custom_mt_uniqid() . "." . $exten_name;//重新命名文件名
+                    $fpath = $filepath;
+                    //路径是否存在，不存在则创建
+                    if (!is_dir($fpath)) mkdir($fpath, 0777);
+                    //调用move_uploaded_file（）函数，进行文件转移
+                    $path = $fpath . $picname;
+                    if (move_uploaded_file($file['tmp_name'], $path)) {
+                        $resultpath = $viewpath . $picname;
+                        $result_data['status'] = 0;
+                        $result_data['msg'] = '成功';
+                        $result_data['data'] = $resultpath;
+                    }
+                } else {
+                    $result_data['msg'] = "该文件不是图片文件";
+                }
             }
-
         } else {
-            $result_data['msg'] = "该文件不是图片文件";
+            $result_data['msg'] = "文件格式错误";
         }
         return $result_data;
     }
